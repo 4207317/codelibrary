4c88481fa369d529b116b0c3032d8f5bf8f53ec4
jinfei_tec <4207317@qq.com>
2017-01-22 17:13:56 +0800

    增加pangu函数

---
+++ lib/pangu_test.php
@@ -0,0 +1,7 @@
+<?php
+/* http://blog.csdn.net/lipengshiwo/article/details/52654728 */
+require "pangu.php";
+
+//echo pangu('Jackie的鼻子有幾個？123個！');   // Jackie 的鼻子有幾個？123 個！
+
+echo pangu("在LeanCloud上，数据存储是围绕AVObject进行的。");
